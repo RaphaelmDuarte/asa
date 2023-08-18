b391194fea395ea9ad33001e92796667d324047d
RaphaelmDuarte <raphaelmotaduarte@gmail.com>
2023-08-18 08:20:06 -0300

    Aula 01 - Inicial

---
+++ Aula_01/src/config.py
@@ -0,0 +1,9 @@
+log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter"
+        }
+    }
+}
