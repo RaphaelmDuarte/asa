1ccfdb755bcc283eb4da70a3e01a5caab3d576af
RaphaelmDuarte <raphaelmotaduarte@gmail.com>
2023-09-01 10:49:59 -0300

    Remoção Aulas

--- Aula_01/src/config.py
+++
@@ -1,9 +0,0 @@
-log_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter"
-        }
-    }
-}
